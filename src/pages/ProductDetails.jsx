a1b16600abbee0567747de52a834f96219d1d963
sracchumi <sergio_21_96@hotmail.com>
2023-11-14 21:34:29 -0300

    borradorCard26

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import { useContext, useEffect } from 'react'
 import { ProductDetail } from '../components/product/ProductDetail'
 import { ProductContext } from '../conexts/ProductContext'
+import CardPolicy from '../components/product/CardPolicy'
 
 export const ProductDetails = () => {
   const { id } = useParams()
@@ -16,15 +17,18 @@ export const ProductDetails = () => {
   console.log(producto)
 
   return (
-    <ProductDetail
-      key={producto.id}
-      categoria={producto.category}
-      nombre={producto.name}
-      marca={producto.brand}
-      precio={producto.price}
-      descripcion={producto.description}
-      caracteristicas={producto.description}
-      imagenes={producto.images}
-    />
+    <div className='Details'>
+      <ProductDetail
+        key={producto.id}
+        categoria={producto.category}
+        nombre={producto.name}
+        marca={producto.brand}
+        precio={producto.price}
+        descripcion={producto.description}
+        caracteristicas={producto.description}
+        imagenes={producto.images}
+      />
+      <CardPolicy/>
+    </div>
   )
 }
