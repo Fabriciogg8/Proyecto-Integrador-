39fba99f676826442fc283909e97e3ad31391670
LeandroCioli <leacioli99@gmail.com>
2023-11-16 19:31:55 -0300

    cambios por galeria de producto

--- src/components/Product/ProductDetail.jsx
+++ src/components/Product/ProductDetail.jsx
@@ -4,10 +4,42 @@ import svgGolden from '../../assets/chevron-left-golden.svg';
 import descLogo from '../../assets/body-text.png'
 import brush from '../../assets/brush.png'
 import prod from '../../assets/producto1.jpg'
+import prod2 from '../../assets/percusion.jpg'
 import { useState } from 'react'
+
+import ImageSlider from '../Product/ImageSlider'
 
 const Detail = () => {
     const [isTrue, setIsTrue] = useState(false)
+
+    const [showG, setShowG] = useState(null);
+    const showGallery = (showG) =>{
+        setShowG(showG);
+      };
+
+    const data = [
+        {
+          image:
+            'https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+        },
+        {
+          image:
+            'https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80'
+        },
+        {
+          image:
+            'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+        },
+        {
+          image:
+            'https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80'
+        },
+        {
+          image:
+            'https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+        }
+      ];
+    
     return (
     <div className='everyDetail'>
         <section className="arrow-title">
@@ -20,8 +52,8 @@ const Detail = () => {
         </section>
         <div>
             <div className='contenedorGalery'>
-                <div className='galeryContainer'>
-                    <img src={prod} className='prodDetailGallery' alt="" />
+                <div className='galeryContainer g-container'>
+                    <img src={prod} className='prodDetailGallery' alt="" onClick={() => showGallery(true)}/>
                     <img src={prod} className='prodDetailGallery' alt="" />
                     <img src={prod} className='prodDetailGallery' alt="" />
                     <img src={prod} className='prodDetailGallery' alt="" />
@@ -29,6 +61,14 @@ const Detail = () => {
                 </div>
             </div>
         </div>
+
+        
+        
+       {<ImageSlider slides={data} show={showG} />}
+        
+        
+
+        
 
         <section className='descripYCaract'>
             <div className='descripcionProd'>
