50b3e1f90c2750db75fb4a0107c35326723a6dbb
SimonPintos7 <89111447+SimonPintos7@users.noreply.github.com>
2023-11-29 23:16:21 -0300

    Update CardPolicy.jsx

--- src/components/product/CardPolicy.jsx
+++ src/components/product/CardPolicy.jsx
@@ -14,8 +14,8 @@ const CardPolicy = () =>{
                     <p>El usuario deberá evitar exponer a los instrumentos a la exposición de la luz solar para evitar su desgaste. Así como también, resguardarlo en su estuche correspondiente</p>
                 </div>
                 <div className="devolucion">
-                    <h5>Devolución</h5>
-                    <p>La devolución del instrumento debe realizarse en la misma condición en la que fue entregado. <br/>El proveedor llevará a cabo una inspección al momento de la devolución.</p>    
+                    <h5>Consultas</h5>
+                    <p>Al momento de realizar consultas con respecto al producto vía Whatsapp, se le garantiza al usuario que la información relativa a la consulta no será hecho pública sin su previo conscentimiento.</p>   
                 </div>
             </div>
         </div>
