8f574ab97c72953e60e11aa926bc1dd4dc8a5d88
sracchumi <sergio_21_96@hotmail.com>
2023-12-05 22:29:56 -0300

    seccion categoria y filtros agregados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 
 import React, { useContext, useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 import CardCategory from '../components/product/CardCategory'
 import Buscador from '../components/buscador/Buscador'
 import BrandSlider from '../components/buscador/BrandSlider'
@@ -12,6 +12,11 @@ import WhatsappButton from '../components/WhatsappButton'
 
 const Home = () => {
   const { state, fetchProducts } = useContext(ProductContext);
+  const [categorias, setCategorias] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  // const [selectedCategory, setSelectedCategory] = useState(null);
+
   useEffect(() => {
     fetchProducts();
   }, []);
@@ -19,10 +24,65 @@ const Home = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const instrumentos = state.products
 
+  
 
-  const categorias = [
+  /*const categorias = [*/
     // ... (código de categorías)
-  ];
+    useEffect(() => {
+      const fetchCategories = async () => {
+        try {
+          const response = await fetch("http://174.129.92.139:8001/api/v1/categories");
+          const data = await response.json();
+          // Actualizar el estado con las categorías obtenidas
+          setCategorias(data);
+        } catch (error) {
+          console.error("Error al obtener categorías:", error);
+        }
+      };
+    
+      fetchCategories();
+    }, [])
+  /*];*/
+  
+  // const handleCategoryClick = (categoryName) => {
+  //   setSelectedCategory(categoryName);
+  // };
+
+  // const filteredProducts = selectedCategory
+  // ? instrumentos.filter((producto) =>
+  //     producto.categories && producto.categories.includes(selectedCategory)
+  //   )
+  // : instrumentos;
+
+  const handleCategoryClick = (categoryName) => {
+    // Verifica si la categoría ya está seleccionada
+    if (selectedCategories.includes(categoryName)) {
+      // Si ya está seleccionada, quítala
+      setSelectedCategories((prevSelected) =>
+        prevSelected.filter((category) => category !== categoryName)
+      );
+    } else {
+      // Si no está seleccionada, agrégala
+      setSelectedCategories((prevSelected) => [...prevSelected, categoryName]);
+    }
+  };
+
+  const clearFilters = () => {
+    // Limpiar los filtros reiniciando el estado de las categorías seleccionadas
+    setSelectedCategories([]);
+  };
+
+  // Filtra los productos según las categorías seleccionadas
+  const filteredProducts = selectedCategories.length > 0
+  ? instrumentos.filter((producto) =>
+      producto.categories && producto.categories.some((category) =>
+        selectedCategories.includes(category)
+      )
+    )
+  : instrumentos;
+
+  const totalProductsCount = instrumentos.length;
+  const filteredProductsCount = filteredProducts.length;
 
   const nextPage = () => {
     if (currentPage < totalPages) {
@@ -47,28 +107,69 @@ const Home = () => {
     <Hero />
     <Buscador />
       <Container>
-      <div className='card-container d-flex flex-row overflow-auto justify-content-start'>
+      {/* <div className='card-container d-flex flex-row overflow-auto justify-content-start'>
         {categorias.length
           ? categorias.map((categoria) => (
               <CardCategory
                 key={categoria.id}
                 name={categoria.name}
-                img={categoria.img}
               />
             ))
           : null}
-      </div>
+      </div> */}
+       <Row className='justify-content-start flex-wrap'>
+          {categorias.length
+            ? categorias.map((categoria) => (
+                <CardCategory key={categoria.id} name={categoria.name} onClick={handleCategoryClick} />
+              ))
+            : null}
+        </Row>
+      
+      {selectedCategories.length > 0 && (
+        <button className="botonFiltro" onClick={clearFilters}>Limpiar Filtros</button>
+      )}
 
+      <p className='leyendaFiltros'>
+        Productos filtrados: {filteredProductsCount} / Total: {totalProductsCount}
+      </p>
 
       {/* Asegúrate de pasar productsPerPage a ProductList */}
-      <ProductList
+      {/* <ProductList
         products={instrumentos}
         currentPage={currentPage}
         nextPage={nextPage}
         prevPage={prevPage}
         goToFirstPage={goToFirstPage}
         productsPerPage={productsPerPage}
-      />
+      /> */}
+        {/* Verificar si hay productos antes de renderizar ProductList */}
+      {/* {filteredProducts.length > 0 && (
+        <ProductList
+          products={filteredProducts}
+          currentPage={currentPage}
+          nextPage={nextPage}
+          prevPage={prevPage}
+          goToFirstPage={goToFirstPage}
+          productsPerPage={productsPerPage}
+        />
+      )} */}
+
+      {/* Mostrar mensaje si no hay productos */}
+      {/* {filteredProducts.length === 0 && (
+        <p>No existen productos para la categoría seleccionada.</p>
+      )} */}
+       {filteredProducts.length > 0 ? (
+        <ProductList
+          products={filteredProducts}
+          currentPage={currentPage}
+          nextPage={nextPage}
+          prevPage={prevPage}
+          goToFirstPage={goToFirstPage}
+          productsPerPage={productsPerPage}
+        />
+      ) : (
+        <p className='leyendaFiltros'>No existen productos para las categorías seleccionadas.</p>
+      )}
 
       <BrandSlider />
       <WhatsappButton/>
