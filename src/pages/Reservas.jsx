6bbb9e41daa23d8507a4a78c9dcfb45695ecdd38
VickydeRebolledo <vdereboleldo@gmail.com>
2023-12-06 14:19:50 -0300

    fron  reserva

--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import { useContext, useEffect } from 'react'
 import { ProductContext } from '../conexts/ProductContext'
 import {DetalleReserva} from '../components/forms/DetalleReserva'
+import '../styles/Reservas.css'
 
 export const Reservas = () => {
     const { id } = useParams()
@@ -14,18 +15,29 @@ export const Reservas = () => {
   }, []);
   const producto = state.currentProduct;
 
-  return(
+  return (
     <div className='reservas'>
-        <ProductDetail
-        key={producto.id}
-        categoria={producto.category}
-        nombre={producto.name}
-        marca={producto.brand}
-        precio={producto.price}
-        descripcion={producto.description}
-        caracteristicas={producto.description}
-        imagenes={producto.images}
-      />
-      <DetalleReserva/>
+      <div className='row align-items-center'>
+        <div className='col-md-5 sub'>
+          {/* Formulario de reserva */}
+          <DetalleReserva />
+        </div>
+        <div className='col-md-6 reservasProducto'>
+          {/* Detalles del producto */}
+          <ProductDetail
+            key={producto.id}
+            categoria={producto.category}
+            nombre={producto.name}
+            marca={producto.brand}
+            precio={producto.price}
+            descripcion={producto.description}
+            caracteristicas={producto.description}
+            imagenes={producto.images}
+          />
+        </div>
+      </div>
     </div>
-)}
+  );
+};
+
+export default Reservas;
